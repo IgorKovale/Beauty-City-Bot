5a56d2e64b7a87d2ab7496cec6a3207de9ef26e3
Ou7ro <qew.qweqwe.2016@mail.ru>
2025-06-27 02:09:36 +0500

    Подчистил код

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from environs import env
-import sqlite3
 from telebot import TeleBot
-from first_message import setup_handlers
+from authorization import setup_handlers
 
 
 env.read_env()
@@ -18,4 +17,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
